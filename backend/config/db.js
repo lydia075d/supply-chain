0a5bf3190826548b3d51a73b160e80f313b78b3d
Priya2217 <163287768+priya2217@users.noreply.github.com>
2026-03-02 12:09:05 +0530

    modified

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose'); 
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
@@ -7,3 +8,4 @@ const connectDB = async () => {
     process.exit(1);
   }
 };
+module.exports = connectDB;
